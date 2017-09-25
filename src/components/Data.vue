68ef8e46597a65c902a0c326ce1ad72e1c0d86f2
zzpengg <cfps91152@gmail.com>
2017-09-25 17:07:03 +0800

    vue starbase on udemy

---
+++ src/components/Data.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="">
+        Type: {{type}}
+        {{items}}
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            type: this.$route.params.type,
+            items: []
+        }
+    },
+    watch: {
+        '$route': 'fetchItems'
+    },
+    methods: {
+        fetchItems() {
+            this.items = [];
+            let initial_ids = [1, 13, 14];
+
+            for(let i in initial_ids){
+                let id = initial_ids[i];
+                fetch(`https://swapi.co/api/${this.type}/${id}`, {
+                    method: 'GET'
+                })
+                    .then(response => response.json())
+                    .then(json => this.items.push(json))
+            }
+        }
+    },
+    created() {
+        this.fetchItems();
+    }
+}
+</script>
